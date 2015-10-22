c27f9f7001d2b6a1460cd0a501966284524ff3d7
Alexander Ose <alex@alexose.com>
2015-10-22 15:23:08 -0400

    change syntax to html

--- correspondence/script.js
+++ correspondence/script.js
@@ -1,7 +1,7 @@
 // This script requires browserify!
 //
 // To build it, just type:
-// browserify script.js
+// browserify -t brfs script.js > assets/js/script.js
 
 var $ = require('jquery');
 var Handlebars = require('handlebars');
@@ -12,8 +12,14 @@ require('brace/mode/javascript');
 require('brace/theme/monokai');
 
 var code = ace.edit('left');
-code.getSession().setMode('ace/mode/javascript');
+code.getSession().setMode('ace/mode/html');
 // editor.setTheme('ace/theme/monokai');
 
 var data = ace.edit('data');
 data.getSession().setMode('ace/mode/json');
+
+// Load example template
+var fs = require('fs');
+var example = fs.readFileSync(__dirname + '/example.html', 'utf8');
+
+code.session.setValue(example);
