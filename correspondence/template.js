69edfeba74e4a1171f1f81c2633a5bdc44fdbc9c
Alexander Ose <alex@alexose.com>
2015-10-29 16:42:09 -0400

    turn template.js into a module

--- correspondence/template.js
+++ correspondence/template.js
@@ -1,47 +1,33 @@
 /* jshint laxcomma:true */
 
-var directory;
-if (process.argv.length == 3){
-  directory = process.argv[2];
-} else {
-  console.log('Usage: node template.js <directory>');
-  process.exit();
-}
+module.exports = function(directory, obj){
 
-var Handlebars = require('handlebars')
-  , fs = require('fs')
-  , local = require('path')
-  , main;
+  var Handlebars = require('handlebars')
+    , fs = require('fs')
+    , local = require('path')
+    , main;
 
-// Scan directory for files and load everything besides index.html as a partial
-var files = fs.readdirSync(directory);
+  // Scan directory for files and load everything besides index.html as a partial
+  var files = fs.readdirSync(directory);
 
-files.forEach(function(path){
-  
-  // Get filename
-  var arr = path.split('/').pop().split('.')
-    , name = arr[0]
-    , ext = arr[1];
+  files.forEach(function(path){
     
-  // Read file
-  var str = fs.readFileSync(local.join(directory, path), 'utf-8');
+    // Get filename
+    var arr = path.split('/').pop().split('.')
+      , name = arr[0]
+      , ext = arr[1];
+      
+    // Read file
+    var str = fs.readFileSync(local.join(directory, path), 'utf-8');
 
-  if (name == 'index'){
-    main = Handlebars.compile(str);
-  } else {
-    Handlebars.registerPartial(name, str);
-    // console.log('Loaded ' + name + ' as a partial');
-  } 
-});
+    if (name == 'index'){
+      main = Handlebars.compile(str);
+    } else {
+      Handlebars.registerPartial(name, str);
+      // console.log('Loaded ' + name + ' as a partial');
+    } 
+  });
 
-// Dummy data
-var data = {
-  test : 'Testing 123',
-  child : true
+  // Render template
+  return main(obj);
 };
-
-// Render template
-var output = main(data);
-
-console.log(output);
-
