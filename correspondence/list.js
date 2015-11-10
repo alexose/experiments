5de101c9d3fea738c72de12dfd170ff8b7b45d4c
Alexander Ose <alex@alexose.com>
2015-11-10 12:15:35 -0500

    update templates

--- correspondence/list.js
+++ correspondence/list.js
@@ -5,30 +5,38 @@ var fs = require('fs');
 module.exports = {
   templates : [
     {
-      name : 'Howdy_letter.html',
+      name : 'Howdy_letter',
       data : fs.readFileSync('./templates/Howdy_letter.html', 'utf-8')
     },
     {
-      name : 'AppointmentNotice.html',
+      name : 'AppointmentNotice',
       data : fs.readFileSync('./templates/AppointmentNotice_confirmation.html', 'utf-8')
     },
     {
-      name : 'Illinois Template.html',
+      name : 'Illinois Template',
       data : fs.readFileSync('./templates/Illinois Template.html', 'utf-8')
     },
     {
-      name : 'NJ_Intro_Demo_Template.html',
+      name : 'NJ_Intro_Demo_Template',
       data : fs.readFileSync('./templates/NJ_Intro_Demo_Template.html', 'utf-8')
+    },
+    {
+      name : 'Real_Letter',
+      data : fs.readFileSync('./templates/Idaho_Letter.html', 'utf-8')
     }
   ],
   partials : [
     {
-      name : 'Standard_Header.html',
+      name : 'Standard_Header',
       data : fs.readFileSync('./templates/Standard_Header.html', 'utf-8')
     },
     {
-      name : 'State_Letterhead.html',
+      name : 'State_Letterhead',
       data : fs.readFileSync('./templates/State_Letterhead.html', 'utf-8')
+    },
+    {
+      name : 'Real_Header',
+      data : fs.readFileSync('./templates/Real_Header.html', 'utf-8')
     }
   ]
 };
