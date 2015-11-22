b57353ffb0f1313a018882484fff54926a2d57d0
Alexander Ose <alex@alexose.com>
2015-11-22 15:53:59 -0500

    attempt react dnd a little more.  I think this plugin is actually terrible

--- packager/scripts/item.js
+++ packager/scripts/item.js
@@ -1,9 +1,31 @@
 var React = require('react');
+var DragSource = require('react-dnd').DragSource;
 
-module.exports = React.createClass({
+var Item = React.createClass({
   render : function(){
-    return(
+
+    var isDragging = this.props.isDragging;
+    var connectDragSource = this.props.connectDragSource;
+
+    console.log(this.props);
+
+    return connectDragSource(
       <li>This is an item!</li>
     )
   }
 });
+
+var source = {
+  beginDrag : function(props){
+    return {};
+  },
+  endDrag : function(props){
+    return {};
+  },
+}
+
+var collect = function(){
+  return {};
+}
+
+module.exports = DragSource('item', source, collect)(Item);
