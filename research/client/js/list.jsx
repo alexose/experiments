f1294e710d5f3d2b64ea0f3624d17d373bd0aebb
Alexander Ose <alex@alexose.com>
2016-03-22 17:57:49 -0400

    make elasticsearch work

--- research/client/js/list.jsx
+++ research/client/js/list.jsx
@@ -20,14 +20,12 @@ var files = [];
 var HitsTable = React.createClass({
   render : function(){
     var hits = this.props.hits;  
-    console.log(this.props.result);
-
     return (
       <table>
         <thead>
           <tr>
             <th scope="col">File</th>
-            <th scope="col">Description</th>
+            <th scope="col">Contents</th>
             <th scope="col">Owner</th>
             <th scope="col">Created</th>
             <th scope="col">Modified</th>
@@ -36,11 +34,25 @@ var HitsTable = React.createClass({
         <tbody>
           {
             hits.map(function(hit){
-              var d = hit._source;
+
+              hit.highlight = hit.highlight || {};
+
+              var d = hit._source,
+                  body = hit.highlight.body || [],
+                  title = hit.highlight.title || d.title;
+
               return (
                 <tr>
-                  <td className={d.type}>{d.title}</td>
-                  <td>{d.description}</td>
+                  <td className={d.type} title={d.description} dangerouslySetInnerHTML={{ "__html" : title }} />
+                  <td>
+                  {
+                    body.map(function(d){
+                      return (
+                        <div dangerouslySetInnerHTML={{ "__html" : d + '...' }} />
+                      )
+                    })
+                  }
+                  </td>
                   <td>{d.owner}</td>
                   <td>{format.date(d.created)}</td>
                   <td>{format.date(d.modified)}</td>
@@ -60,11 +72,14 @@ module.exports = React.createClass({
       <div> 
         <SearchkitProvider searchkit={sk}>
           <div className="search">
-              <SearchBox/>
+              <SearchBox
+                searchOnChange={true}
+                queryOptions={{analyzer:"standard"}} 
+                queryFields={["title", "body"]} />
               <Hits 
                 hitsPerPage={50} 
                 highlightFields={["title", "body"]} 
-                sourceFilter={["title", "owner", "size", "description", "modified"]} 
+                sourceFilter={["title", "owner", "size", "description", "modified", "result", "highlight"]}
                 listComponent={HitsTable} />
           </div>
         </SearchkitProvider>
