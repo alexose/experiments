cad165a9b6998746efa1efe4f2eb7421781c2d9f
Alexander Ose <alex@alexose.com>
2016-03-22 18:14:41 -0400

    table width

--- research/client/utils/format.js
+++ research/client/utils/format.js
@@ -9,17 +9,11 @@ function formatDate() {
 
   var month = date.getMonth() + 1;
   var day = date.getDate();
-  var hour = date.getHours();
-  var min = date.getMinutes();
-  var sec = date.getSeconds();
 
   month = (month < 10 ? "0" : "") + month;
   day = (day < 10 ? "0" : "") + day;
-  hour = (hour < 10 ? "0" : "") + hour;
-  min = (min < 10 ? "0" : "") + min;
-  sec = (sec < 10 ? "0" : "") + sec;
 
-  var str = date.getFullYear() + "-" + month + "-" + day + " " +  hour + ":" + min + ":" + sec;
+  var str = date.getFullYear() + "-" + month + "-" + day;
 
   return str;
 }
